ec507835c5935a2d974ef13a1f9dc9e7d918b5bd
janranz <57410440+janranz@users.noreply.github.com>
2020-09-08 12:24:29 -0700

    CS175 Web ToDo update

--- CS175/WK3/simpleToDo/README.md
+++ CS175/WK3/simpleToDo/README.md
@@ -1,10 +1,20 @@
 ## To actually do:
 ---
 
-- Figure out how to self-invoke or call function to clear / reset fields upon page load. [ toggleDisable(); ]
-- Set up results table. Since page is (hopefully) static, might need some way of refreshing table or a listener to display results. Start with figuring out what javascript:void(0) is *actually* doing in form action.
-- Add feature for user to delete a class from storage. Add input for Class ID and search array.
-- Consider a way to dump entire array (is the local storage used dealloc'd in some way?)
-- Figure out formatting the table / form (CSS)
+- ~~Figure out how to self-invoke or call function to clear / reset fields upon page load. [ toggleDisable(); ]~~
+- ~~Set up results table. Since page is (hopefully) static, might need some way of refreshing table or a listener to display results. Start with figuring out what javascript:void(0) is *actually* doing in form action.~~
+- ~~Add feature for user to delete a class from storage. Add input for Class ID and search array.~~
+- ~~Consider a way to dump entire array (is the local storage used dealloc'd in some way?)~~
+- ~~Figure out formatting the table / form (CSS)~~
 
+---
+- Functioning, but most likely not optimized. Something I can reflect on later as I get more familiar with these languages.
+- Lacks style, but that is the least of my worries *right now*. Maybe some day... when I can intuitively float elements and know all the colors of the wind.
+- Also going on the first bullet. Surely some corner case will break this and I didn't dive too deep in preventing them:
+    * forces required inputs / time
+    * forces class ID to uppercase (key)
+    * populates DB from local storage, and re\-draws after almost every function call
+    * packs inputs into an array of strings, which are then converted and passed on as key/value pairs for ease of access
+    * Manually sets Lab fields to a '\-dash\-' over an empty string if checkbox is unchecked
+    * forces upperCase to 'Delete Class' field to avoid key mis-match (If it's even case-sensitive to begin with).
 ---
--- CS175/WK3/simpleToDo/toDo.js
+++ CS175/WK3/simpleToDo/toDo.js
@@ -1,25 +1,21 @@
 "use strict";
 
-
 $(document).ready(function()
 {
     function toggleLabCheck()
     {
-        let dis = $("#lRoom").prop('disabled');
-        console.log(dis);
         // console.log("wow such toggle.");
         if(this.checked)
         {
-            // console.log("Visible");
-            // $("#labSec").fadeIn();
+            $("#labSec").fadeIn();
             toggleDisable("#labSec :input", false);
         } else
         {
             toggleDisable("#labSec :input",true);
-            // $("#labSec").fadeOut(); // check that this is not submitting fields
-            // $("#labSec :input").each(function(){
-            //     $(this).prop('disabled', true);
-            // });
+            $("#labSec").fadeOut();
+            $("#labSec :input").each(function(){
+                $(this).prop('disabled', true);
+            });
 
         }
     }
@@ -29,6 +25,103 @@ $(document).ready(function()
             $(this).prop("disabled", b)
         });
     }
-    (function(){toggleDisable("labSec :input",true);});
+    // add-remove functions
+    function addStudy(e)
+    {
+        e.preventDefault()
+        var classPack = [];
+        var keyID = $("#cID").val();
+        console.log("ID and type: ",keyID, typeof(keyID))
+        var userInputs = $(".schedForm :input").not('#cID,:input[type=button],:input[type=submit],:input[type=reset]'); // skips userID length:8
+        var labBool = userInputs.eq(4)[0].checked;
+        console.log("userInputs",userInputs);
+        for(let m = 0; m < userInputs.length ;m++)
+        {
+            var smol = userInputs.eq(m).val()
+            if(m == 4 && labBool)
+            {
+                classPack.push("YES");
+                continue;
+            }else if(m == 4 && !labBool)
+            {
+                classPack.push("NO");
+                for(let c = 5; c < userInputs.length; c++)
+                {
+                    classPack.push("-");
+                }
+                break;
+            }
+            classPack.push(smol);
+        }
+        console.log("classPack: ",classPack);
+        storeLocal(keyID.toUpperCase(),classPack);
+        newUpdateDB();
+    }
+    function newUpdateDB()
+    {
+        $("#schedTab").DataTable().clear().draw();
+        for(var m = 0; m < localStorage.length; m++)
+        {
+            var tempKey = localStorage.key(m);
+            var tempDict = JSON.parse(localStorage.getItem(tempKey))
+            $("#schedTab").DataTable().row.add([tempKey,tempDict[0],tempDict[1],tempDict[2],tempDict[3],tempDict[4],tempDict[5],tempDict[6],tempDict[7],tempDict[8]]).draw();
+        }
+    }
+    function removeStudy()
+    {
+        var userRemove = $("#remID").val().toUpperCase();
+        console.log(userRemove);
+        if(!(localStorage.getItem(userRemove) === null))
+        {
+            localStorage.removeItem(userRemove);
+            newUpdateDB();
+            $("#delNotif").html("Class successfully removed.");
+        } else
+        {
+            $("#delNotif").html("Invalid Class ID.");
+        }
+    }
+    //localStorage functions
+    function storeLocal(k,v)
+    {
+        localStorage.setItem(k,JSON.stringify(v));
+    }
+    function clearLocal()
+    {
+        if(window.confirm("This will clear both local storage and table."))
+        {
+            console.log("clearing local storage...");
+            localStorage.clear();
+            $("#schedTab").DataTable().clear().draw();
+        } else{console.log("clear aborted by user");}  
+    }
+    // 'global' ops
+    $("#schedTab").DataTable();
+    toggleDisable("#labSec :input",true);
+    $(".schedForm").on('submit',function(e)
+    {
+        addStudy(e);
+    });
+    $(".schedForm")[0].reset();
+    $("#remID").val('');
+    $("#delNotif").html('');
+    newUpdateDB();
+    $("#remClassByID").on("click",removeStudy);
+    $("#clearStore").on("click",clearLocal);
     $("#labCheck").on("change",toggleLabCheck);
 });
+
+/*
+Information being stored:
+[ 9 COLUMNS ] Consider[][]?
+Class ID | Class Title | Start Time | Finish Time | Class Room | Is Lab?
+                                    | Lab Start   | Lab Finish | Lab Room
+
+
+    // function updateDB(k)
+    // {
+    //     var testPull = JSON.parse(localStorage.getItem(k));
+    //     // console.log("Test pull: ", testPull, testPull[2], typeof(testPull[1]));
+    //     $("#schedTab").DataTable().row.add([k,testPull[0],testPull[1],testPull[2],testPull[3],testPull[4],testPull[5],testPull[6],testPull[7],testPull[8]]).draw();
+    // }
+*/
